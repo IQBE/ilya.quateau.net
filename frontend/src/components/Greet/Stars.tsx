f52b7648a3bf40bb66fc5069d42637966b22379e
Ilya Quateau <git@quateau.net>
2025-10-30 15:45:34 +0100

    Added falling stars

--- frontend/src/components/Greet/Stars.tsx
+++ frontend/src/components/Greet/Stars.tsx
@@ -8,11 +8,38 @@ const Stars = () => {
     duration: `${5 + Math.random() * 5}s`,
   }));
 
+  const fallingStars = Array.from({ length: 5 }, (_, i) => {
+    const segmentWidth = 100 / 5;
+    const horizontalBase = i * segmentWidth;
+    const horizontalOffset = Math.random() * segmentWidth;
+    const top = Math.random() * 80;
+
+    return {
+      top: `${top}%`,
+      left: `${horizontalBase + horizontalOffset}%`,
+      delay: `${Math.random() * 5}s`,
+      duration: `${3 + Math.random() * 7}s`,
+    };
+  });
+
   return (
     <div className={styles.stars}>
       {stars.map((star, index) => (
         <span
           key={index}
+          style={{
+            top: star.top,
+            left: star.left,
+            animationDelay: star.delay,
+            animationDuration: star.duration,
+          }}
+        />
+      ))}
+
+      {fallingStars.map((star, index) => (
+        <span
+          key={`falling-${index}`}
+          className={styles.falling}
           style={{
             top: star.top,
             left: star.left,
